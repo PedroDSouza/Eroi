087ec4105a719e7d19f9a198e27d5a3b634eb880
Pedro de Souza <pedrohodesouza@gmail.com>
2022-11-24 09:37:47 -0300

    Insere o Projeto Inicial

---
+++ routes/web.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+
+    $nome = "Pedrov";
+    $idade = 22;
+
+    return view('welcome', ['nome' => $nome, 'idade' => $idade]);
+});
+
+Route::get('/principal', function () {
+
+    $nome = "Pedro de Souza";
+    $ano = 2022;
+    
+    return view('pagina-principal', ['nome' => $nome, 'ano' => $ano]);
+});
