dd176ec3f8db3a59976c11efe0a40ef1102bd68e
Pedro de Souza <pedrohodesouza@gmail.com>
2022-12-07 11:54:15 -0300

    Cria tela básica de login

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -12,7 +12,18 @@
   <link rel="icon" type="image/x-icon" href="/images/IMUNIDADE.png">
 
   <!-- Font Awesome -->
-  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.11.2/css/all.css">
+  <!-- <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.11.2/css/all.css"> -->
+
+  <!-- Google Fonts -->
+
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300&display=swap" rel="stylesheet">
+
+  <!-- END Google Fonts -->
+
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+
   <!-- Bootstrap core CSS -->
   <link href="../css/bootstrap.min.css" rel="stylesheet">
   <!-- Material Design Bootstrap -->
@@ -197,6 +208,12 @@
   <!-- Container wrapper -->
 </nav>
 <!-- Navbar -->
+
+<div>
+          <br>
+          <br>
+          <br>
+        </div>
 
 @yield('content')
 
